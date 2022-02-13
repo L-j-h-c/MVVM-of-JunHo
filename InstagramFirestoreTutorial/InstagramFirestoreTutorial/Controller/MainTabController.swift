a62aaef3d5cd098c300b6576a1f9ce9d0bc217a2
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-13 18:30:41 +0900

    [Chore] #1 - VC 생성 및 탭바에 연결
    VC 생성 및 탭바에 연결

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class MainTabController: UITabBarController {
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewControllers()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureViewControllers() {
+        view.backgroundColor = .white
         
-        view.backgroundColor = .red
+        // 뷰컨트롤러들의 인스턴스를 만든 것
+        let feed = FeedController()
+        
+        let search = SearchController()
+        
+        let imageSelector = ImageSelectorController()
+        
+        let notifications = NotificationController()
+        
+        let profile = ProfileController()
+        
+        viewControllers = [feed, search, imageSelector, notifications, profile]
     }
 }
---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/SearchController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Junho Lee on 2022/02/13.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
