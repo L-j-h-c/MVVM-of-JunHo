f719c1aed7fcc2014afe292008bc36b90a1ab499
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-16 04:21:40 +0900

    [Feat] #3 - 프로필 네임 버튼 추가
    프로필 네임 버튼 추가

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
@@ -20,6 +20,15 @@ class FeedCell: UICollectionViewCell {
         return iv
     }()
     
+    private lazy var usernameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -31,9 +40,20 @@ class FeedCell: UICollectionViewCell {
         profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
         profileImageView.setDimensions(height: 40, width: 40)
         profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(usernameButton)
+        usernameButton.centerY(inView: profileImageView,
+                               leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Actions
+    
+    @objc func didTapUsername() {
+        print("DeBUG")
+    }
+
 }
