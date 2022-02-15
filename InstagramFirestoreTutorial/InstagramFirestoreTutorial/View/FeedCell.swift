24c221c390589a2bd08b806ad2be304cd85ee66c
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-16 04:14:58 +0900

    [Feat] #3 - 익스텐션 추가 및 프로필 이미지 생성
    익스텐션 추가 및 프로필 이미지 생성

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
@@ -11,12 +11,26 @@ class FeedCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = UIImage(named: "venom-7")
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemPurple
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        profileImageView.setDimensions(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
     }
     
     required init?(coder: NSCoder) {
