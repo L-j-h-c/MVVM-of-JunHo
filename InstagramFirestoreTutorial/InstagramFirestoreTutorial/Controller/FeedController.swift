d13a76fd2b1b7f220199054d0352ad199fe882b9
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-16 03:55:27 +0900

    [Feat] #3 - collectionViewController로 컬렉션뷰 구성
    collectionViewController로 컬렉션뷰 구성

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/FeedController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/FeedController.swift
@@ -7,11 +7,44 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+private let reuseIdentifier = "cell"
+
+class FeedController: UICollectionViewController {
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: -Helpers
+    
+    func configureUI() {
+        collectionView.backgroundColor = .white
         
-        view.backgroundColor = .systemBlue
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: UICollectionViewDataSource
+
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
 }
