eda388d8e4e2d492d4487780e2f16cca4ac2f369
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-15 20:11:56 +0900

    [Feat] #1 - 내비바 세팅
    내비바 세팅

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -22,16 +22,37 @@ class MainTabController: UITabBarController {
         view.backgroundColor = .white
         
         // 뷰컨트롤러들의 인스턴스를 만든 것
-        let feed = FeedController()
+        let feed = templateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"), rootViewController: FeedController())
         
-        let search = SearchController()
+        let search = templateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"), rootViewController: SearchController())
         
-        let imageSelector = ImageSelectorController()
+        let imageSelector = templateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"), rootViewController: ImageSelectorController())
         
-        let notifications = NotificationController()
+        let notifications = templateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"), rootViewController: NotificationController())
         
-        let profile = ProfileController()
+        let profile = templateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"), rootViewController: ProfileController())
         
         viewControllers = [feed, search, imageSelector, notifications, profile]
+        
+        tabBar.tintColor = .black
+        tabBar.backgroundColor = .white
+        
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithDefaultBackground()
+
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
+    }
+    
+    func templateNavigationController(unselectedImage: UIImage?, selectedImage: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        nav.navigationBar.backgroundColor = .white
+        return nav
     }
 }
