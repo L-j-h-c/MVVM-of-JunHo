05de2784c310fd2c522f5d84e736fbbc69cdfae8
L-j-h-c <ckrgkswnsgh@naver.com>
2022-03-01 16:44:27 +0900

    [Feat] #4 - 파이어베이스에 유저 등록하기
    파이어베이스에 유저 등록하기

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/API/AuthService.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/API/AuthService.swift
@@ -5,8 +5,8 @@
 //  Created by Junho Lee on 2022/03/01.
 //
 
-import Foundation
 import UIKit
+import Firebase
 
 struct AuthCredentials {
     let email: String
@@ -17,7 +17,25 @@ struct AuthCredentials {
 }
 
 struct AuthService {
-    static func registerUser(withCredential credentials: AuthCredentials) {
-        print("DEBUG: Crednetials are \(credentials)")
+    static func registerUser(withCredential credentials: AuthCredentials, completion: @escaping(Error?) -> Void) {
+        
+        ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { (result, error) in
+                if let error = error {
+                    print("DEBUG: Failed to register user(error.localizedDescription)")
+                    return
+                }
+                
+                guard let uid = result?.user.uid else { return }
+                
+                let data: [String: Any] = ["email": credentials.email,
+                                           "fullname": credentials.fullname,
+                                           "profileImageUrl": imageUrl,
+                                           "uid": uid,
+                                           "username": credentials.username]
+                
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
     }
 }
--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -72,12 +72,19 @@ class RegistrationController: UIViewController {
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         guard let fullname = fullnameTextField.text else { return }
-        guard let username = usernameTextField.text else { return }
+        guard let username = usernameTextField.text?.lowercased() else { return }
         guard let profileImage = self.profileImage else { return }
         
         let credentials = AuthCredentials(email: email, password: password, fullname: fullname, username: username, profileImage: profileImage)
         
-        AuthService.registerUser(withCredential: credentials)
+        AuthService.registerUser(withCredential: credentials) { error in
+            if let error = error {
+                print("DEBUG: Failed to register user(error.localizedDescription)")
+                return
+            }
+            
+            print("DEBUG: Succefully registered user with firestore..")
+        }
     }
     
     @objc func handleShowSignUp() {
